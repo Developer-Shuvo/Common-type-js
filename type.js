34ffbfe92869d77880360d4a13817f1ae6381327
SHUVO <im.computer.engineeer@gamil.com>
2025-05-13 14:41:56 +0600

    Primitive & Non primitive data type exploring

--- type.js
+++ type.js
@@ -2,3 +2,13 @@ let a = 10;
 let b = a;
 b = 50;
 console.log (a,b);
+
+
+// ______________________________________________________________________________________
+
+let student = { name: "john", Passed : true};
+let teacher = student;
+teacher = { name: 'Kamal sir', experience: 10};
+
+
+console.log (student, teacher);
